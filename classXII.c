dac29da45671ad7229f3849ab65412eef46ca86a
Arjun Rokkha <arjunrokka@gmail.com>
2021-11-25 22:42:43 +0545

    update

--- classXII.c
+++ classXII.c
@@ -1,6 +1,23 @@
-#include<stdi.h>
-#include<conio.h>
-int main(){
-printf("Hello World");
-return 0;
+#include<stdio.h>
+int main()
+{
+    int i, n, lar,sm, elem;
+    printf ("Enter total number of elements : ");
+    scanf ("%d", &elem);
+    printf ("Enter first number : ");
+    scanf ("%d", &n);
+    lar = n;
+    sm=n;
+    for (i=1; i<= elem -1 ; i++)
+    {
+        printf ("\n Enter another number : ");
+        scanf ("%d",&n);
+        if (n>lar)
+        lar=n;
+        if (n<sm)
+        sm=n;
+    }
+    printf ("\n The largest number is %d", lar);
+    printf ("\n The smallest number is %d", sm);
+    return 0;
 }
