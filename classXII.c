2bc1c7868ad493402c1784fba48545ffdd8f2ab4
Arjun Rokkha <arjunrokka@gmail.com>
2021-12-03 20:40:56 +0545

    updated

--- classXII.c
+++ classXII.c
@@ -106,25 +106,33 @@
 
 
 
-// A c program to assign values to the member of structure and to display them on the screen
-#include <stdio.h>
-// #include <conio.h>
-#include <string.h>
-struct student
-{
-    char name[10];
-    unsigned int roll_no;
-    float mark;
-};
-int main()
-{
-    struct student stu1;
-    clrscr();
-    strcpy(stu1.name, "Arjun");
-    stu1.roll_no = 1;
-    stu1.mark = 98;
-    printf("\n The name of student is %s ", stu1.name);
-    printf("\n The roll_no %u", stu1.roll_no);
-    printf("\n The mark obtained id %f", stu1.mark);
-    return 0;
-}
+ // A c program to assign values to the member of structure and to display them on the screen
+// #include <stdio.h>
+ // #include <conio.h>
+// #include <string.h>
+// struct student
+// {
+//     char name[10];
+//     unsigned int roll_no;
+//     float mark;
+// };
+// int main()
+// {
+//     struct student stu1;
+//     clrscr();
+//     strcpy(stu1.name, "Arjun");
+//     stu1.roll_no = 1;
+//     stu1.mark = 98;
+//     printf("\n The name of student is %s ", stu1.name);
+//     printf("\n The roll_no %u", stu1.roll_no);
+//     printf("\n The mark obtained id %f", stu1.mark);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// #include<conio.h>
+// // clscr();
+// void main(){
+//     printf("Arjun Rokkha");
+// }
